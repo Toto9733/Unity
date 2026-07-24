8d700937f26826a10f9c23b1f8158e5c589ee0d6
Toto9733 <felientoto@gmail.com>
2026-07-24 02:32:30 +0200

    Update shop_config_setrole.js

--- src/commands/Economy/modules/shop_config_setrole.js
+++ src/commands/Economy/modules/shop_config_setrole.js
@@ -1,14 +1,15 @@
+```javascript
 import { PermissionsBitField } from 'discord.js';
 import { successEmbed } from '../../../utils/embeds.js';
 import { getGuildConfig, setGuildConfig } from '../../../services/config/guildConfig.js';
 import { InteractionHelper } from '../../../utils/interactionHelper.js';
 import { logger } from '../../../utils/logger.js';
-
 import { replyUserError, ErrorTypes } from '../../../utils/errorHandler.js';
+
 export default {
     async execute(interaction, config, client) {
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
-            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'You need **Manage Server** permissions to set the premium role.' });
+            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'Tu as besoin de la permission **Gérer le serveur** pour définir le rôle premium.' });
         }
 
         const role = interaction.options.getRole('role');
@@ -20,12 +21,14 @@ export default {
             await setGuildConfig(client, guildId, currentConfig);
 
             return InteractionHelper.safeReply(interaction, {
-                embeds: [successEmbed('Premium Role Set', `The **Premium Shop Role** has been set to ${role.toString()}. Members who purchase the Premium Role item will be granted this role.`)],
+                embeds: [successEmbed('Rôle premium défini', `Le **rôle de la boutique premium** a été défini sur ${role.toString()}. Les membres qui achèteront l'article du rôle premium se verront attribuer ce rôle.`)],
                 ephemeral: true,
             });
         } catch (error) {
-            logger.error('shop_config_setrole error:', error);
-            return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'Could not save the guild configuration.' });
+            logger.error('Erreur shop_config_setrole :', error);
+            return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'Impossible d\'enregistrer la configuration du serveur.' });
         }
     },
 };
+
+```
