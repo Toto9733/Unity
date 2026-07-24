90e168681564cc203bcbeea61cda69d60d16e93b
Toto9733 <felientoto@gmail.com>
2026-07-25 00:34:16 +0200

    Update leaderboard.js

--- src/commands/Leveling/leaderboard.js
+++ src/commands/Leveling/leaderboard.js
@@ -6,7 +6,7 @@ import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('classement')
+    .setName('leaderboard')
     .setDescription("Affiche le classement des niveaux du serveur")
     .setDMPermission(false),
   category: 'Niveaux',
